b47c73e1ff552c7516a7b48b33580936996ce540
David Barkemeyer <david.barkemeyer@gmx.de>
2021-11-20 18:02:17 +0100

    Minor fixes

--- Assets/Prefabs/DialogueManager/DialogueTrigger.cs
+++ Assets/Prefabs/DialogueManager/DialogueTrigger.cs
@@ -2,15 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class DialogueTrigger : MonoBehaviour
 {
     public Dialogue dialogue;
+    public Button prevButton;
     public bool fireOnSceneLoad;
 
     public void TriggerDialogue()
     {
-        DialogueManager.GetInstance().StartDialogue(dialogue);
+        DialogueManager.GetInstance().StartDialogue(dialogue, prevButton);
     }
 
     // called first
